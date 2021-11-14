e3af364cef15fad37239cd30488313d04769b088
yuyatabata <tabayu5@gmail.com>
2021-11-15 08:38:31 +0900

    show received answer

--- src/pages/questions/[id].tsx
+++ src/pages/questions/[id].tsx
@@ -16,6 +16,7 @@ import {
 import Layout from "../../components/Layout";
 import { Question } from "../../models/Question";
 import { useAuthentication } from "../../hooks/authentication";
+import { Answer } from "../../models/Answer";
 
 type Query = {
   id: string;
@@ -27,6 +28,7 @@ const QuestionsShow = () => {
   const { user } = useAuthentication();
   const [question, setQuestion] = useState<Question>(null);
   const [isSending, setIsSending] = useState(false);
+  const [answer, setAnswer] = useState(null);
   const [body, setBody] = useState("");
 
   const getCollections = () => {
@@ -52,6 +54,25 @@ const QuestionsShow = () => {
     const gotQuestion = questionDoc.data() as Question;
     gotQuestion.id = questionDoc.id;
     setQuestion(gotQuestion);
+
+    if (!gotQuestion.isReplied) {
+      return;
+    }
+
+    const answerSnapshot = await getDocs(
+      query(
+        answersCollection,
+        where("questionId", "==", gotQuestion.id),
+        limit(1)
+      )
+    );
+    if (answerSnapshot.empty) {
+      return;
+    }
+
+    const gotAnswer = answerSnapshot.docs[0].data() as Answer;
+    gotAnswer.id = answerSnapshot.docs[0].id;
+    setAnswer(gotAnswer);
   };
 
   useEffect(() => {
@@ -77,46 +98,60 @@ const QuestionsShow = () => {
       });
     });
 
-    setIsSending(false);
+    const now = new Date().getTime();
+    setAnswer({
+      id: "",
+      uid: user.uid,
+      questionId: question.id,
+      body,
+      createdAt: new Timestamp(now / 1000, now % 1000),
+    });
   };
 
   return (
     <Layout>
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-6">
-          {question && (
+      <div className="col-12 col-md-6">
+        {question && (
+          <>
             <div className="card">
               <div className="card-body">{question.body}</div>
             </div>
-          )}
-        </div>
-      </div>
-      <section className="text-center mt-4">
-        <h2 className="h4">回答する</h2>
 
-        <form onSubmit={onSubmit}>
-          <textarea
-            className="form-control"
-            placeholder="おげんきですか？"
-            rows={6}
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-            required
-          ></textarea>
-          <div className="m-3">
-            {isSending ? (
-              <div
-                className="spinner-border text-secondary"
-                role="status"
-              ></div>
-            ) : (
-              <button type="submit" className="btn btn-primary">
-                回答する
-              </button>
-            )}
-          </div>
-        </form>
-      </section>
+            <section className="text-center mt-4">
+              <h2 className="h4">回答</h2>
+
+              {answer === null ? (
+                <form onSubmit={onSubmit}>
+                  <textarea
+                    className="form-control"
+                    placeholder="おげんきですか？"
+                    rows={6}
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    required
+                  ></textarea>
+                  <div className="m-3">
+                    {isSending ? (
+                      <div
+                        className="spinner-border text-secondary"
+                        role="status"
+                      ></div>
+                    ) : (
+                      <button type="submit" className="btn btn-primary">
+                        回答する
+                      </button>
+                    )}
+                  </div>
+                </form>
+              ) : (
+                <div className="card">
+                  <div className="card-body text-left">{answer.body}</div>
+                </div>
+              )}
+            </section>
+          </>
+        )}
+      </div>
     </Layout>
   );
 };
